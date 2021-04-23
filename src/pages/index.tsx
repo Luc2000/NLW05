2640a84cb978fda63f19d853d45f630c0e433c13
Lucas  Souza Annunziato <lucassouzaannunziato@gmail.com>
2021-04-23 00:35:05 -0300

    Aula 03

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,145 @@
-// SPA - Single Page Application 
-// SSR - Server Side Rendering
-// SSG - Static Side Generation
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
- 
-//USANDO SSG - Geração estática da página, gerado a partir do 1° Acesso, evita consultar muito a API ------------------------------------------------------
-export default function Home(props) {
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
+
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[];
+  allEpisodes: Episode[];
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+
   return (
-    <div>
-      <h1>Index</h1>
-      <p>{JSON.stringify(props.episodes)}</p>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                  objectFit="cover"
+                />
+
+                <div className={styles.episodeDetails}>
+                  <Link href={`/episodes/${episode.id}`}>
+                    <a >{episode.title}</a>
+                  </Link>
+                  <p>{episode.members}</p>
+                  <span>{episode.publishedAt}</span>
+                  <span>{episode.durationAsString}</span>
+                </div>
+                <button type="button">
+                  <img src="/play-green.svg" alt="Tocar episódio" />
+                </button>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => {
+              return (
+                <tr key={episode.id}>
+                  <td style={{ width: 72 }}>
+                    <Image
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar episódio" />
+                    </button>
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </section>
     </div>
   )
 }
 
-// Unico contra -> Ele vai executar toda vez que alguém acessar nossa página.
-export async function getStaticProps() {  //Só de a função ter esse nome, o Next já entende que tem que executar antes de mostrar o conteudo pro usuário.
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
-
-  return{
-    props: {    //Tem que vir como uma propriedade.
-      episodes:data,
-    },
-    revalidate: 60*60*8,    //Ou seja, durante 8 horas a página vai ser igual.
-    //Exemplo: João acessa a página 10:20, se maria acessar a mesma página as 15:00, a página será o mesmo HTML estático, não precisa consultar a API para que maria veja a página
-  }
-}
-
-
-/* 
-//USANDO SPA - REACT TRADICIONAL ------------------------------------------------------
-import { useEffect } from "react"
-
-export default function Home() {
-    useEffect(() => {
-      fetch('http://localhost:3333/episodes')
-        .then(response => response.json())
-        .then(data => console.log(data))
-
-    }, [])   //Dispara quando ALGO mudar, efeito colateral. ->Unico contra, essas infos não são carregadas de primeira, elas vem do JS
-  
-  return (
-    <h1>Index</h1>
-  )
-}
-*/
-
-/*
-//USANDO SSR - Next.js ------------------------------------------------------------------
-export default function Home(props) {
-  console.log(props.episodes)
-  return (
-    <h1>Index</h1>
-  )
-}
-
-// Unico contra -> Ele vai executar toda vez que alguém acessar nossa página.
-export async function getServerSideProps() {  //Só de a função ter esse nome, o Next já entende que tem que executar antes de mostrar o conteudo pro usuário.
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json()
-
-  return{
-    props: {    //Tem que vir como uma propriedade.
-      episodes:data,
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
     }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      url: episode.file.url
+    }
+  })
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes
+    },
+    revalidate: 60 * 60 * 8,
   }
 }
-*/
-
-
-
-
